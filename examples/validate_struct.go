60437511b886c71ec5f102acc139166faed1f6d6
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-07-10 22:03:10 -0300

    refactor: add json tags to example struct

--- examples/validate_struct.go
+++ examples/validate_struct.go
@@ -7,9 +7,9 @@ import (
 )
 
 type User struct {
-	Username string `validate:"required,min=6,max=32"`
-	Email    string `validate:"required,email"`
-	Password string `validate:"required,min=8,max=32"`
+	Username string `validate:"required,min=6,max=32" json:"username"`
+	Email    string `validate:"required,email" json:"email"`
+	Password string `validate:"required,min=8,max=32" json:"password"`
 }
 
 func ValidateStruct() {
